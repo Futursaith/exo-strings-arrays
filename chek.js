f89c4d0d00b22949bf089551394bea971153296f
Futursaith <saith_7@hotmail.fr>
2021-02-17 18:34:32 +0100

    exo1

---
+++ chek.js
@@ -0,0 +1,6 @@
+const text = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript'
+const obj = 'Javascript'
+
+console.log(`${text.includes(obj) ? 'javascript est  présent dans le texte' : 'Le mot Javascript n\'est pas présent'}`)
+
+
